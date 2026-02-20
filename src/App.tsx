c7728dbcc1917bd087f0b51561da34b10935fa14
levith <levith.dev@gmail.com>
2026-02-20 13:59:01 -0300

    feat: add text inputs and word counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-
+import { useState } from "react"
 
 function App() {
+    const [beforeText, setBeforeText] = useState("");
+    const [afterText, setAfterText] = useState("");
+
+  const countWords = (text :string): number => {
+    const trimmed = text.trim();
+    return trimmed === "" ? 0 : trimmed.split(/\s+/).length;
+  }
 
   return (
     <div>
-      <p>
+      <div>
+        <input
+         type="text"
+         value={beforeText}
+         onChange={(e) => setBeforeText(e.target.value)}
+         />
 
-      </p>
+         <div>
+          <p>palavras: {countWords(beforeText)}</p>
+         </div>
+      </div>
+      <div>
+        <input 
+        type="text"
+        value={afterText}
+        onChange={(e) => setAfterText(e.target.value)}
+        />
+        <p>palavras: {countWords(afterText)}</p>
+      </div>
     </div>
   )
 }
