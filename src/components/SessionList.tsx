a3c62978a4b0ab04493f29d23715168381969204
levith <levith.dev@gmail.com>
2026-03-04 20:22:05 -0300

    style(session-list): add gap color, adjust layout and typography

--- src/components/SessionList.tsx
+++ src/components/SessionList.tsx
@@ -19,7 +19,11 @@ export interface SessionListProps {
     countWords: (text: string) => number
   }
 }
-
+const getGapColor = (gap: number) => {
+  if (gap < 0) return "text-red-600"
+  if (gap === 0) return "text-gray-600"
+  return "text-green-950"
+}
 export function SessionList({
   data,
   actions
@@ -40,44 +44,59 @@ export function SessionList({
     <div>
       <ul>
         {pomoList.map((pomo) => (
-          <li key={pomo.time}>
-            {editingTime === pomo.time ? (
-              <input
-                type="text"
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-                onKeyDown={(e) =>
-                  handleEnter(e, () => {
-                    rename(pomo.time)
-                    setEditingTime(null)
-                    setNewName("")
-                  })
-                }
-              />
-            ) : (
-              <h3 onClick={() => taskInEdit(pomo.time)}> {pomo.name}</h3>
-            )}
+          <li key={pomo.time} >
+            <div className="bg-slate-500 border border-black m-4 rounded-lg flex flex-col justify-between">
+              <div className="flex justify-between items-center">
+                {editingTime === pomo.time ? (
+                  <input
+                    className=""
+                    type="text"
+                    value={newName}
+                    onChange={(e) => setNewName(e.target.value)}
+                    onKeyDown={(e) =>
+                      handleEnter(e, () => {
+                        rename(pomo.time)
+                        setEditingTime(null)
+                        setNewName("")
+                      })
+                    }
+                  />
+                ) : (
+                  <h3
+                    className="text-  xl font-bold"
+                    onClick={() => taskInEdit(pomo.time)}> {pomo.name}</h3>
+                )}
+                <div className="flex flex-col ">
+                  <p className="flex justify-end text-sm">
+                    {new Date(pomo.time).toLocaleDateString("pt-BR", {
+                      day: "2-digit",
+                      month: "2-digit",
+                      year: "numeric",
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </p>
 
-            <div>
-              <p>Before: {countWords(pomo.beforeText)}</p>
-              <p>After: {countWords(pomo.afterText)}</p>
-              <p>Gap: {pomo.wordGap}</p>
+
+                </div>
+
+              </div>
+              <div>
+                <p>Before: {countWords(pomo.beforeText)}</p>
+                <p>After: {countWords(pomo.afterText)}</p>
+                <p className={getGapColor(pomo.wordGap)}>{pomo.wordGap}</p>
+              </div>
+              <div className="flex justify-end ">
+                <button
+                  className=""
+                  onClick={() => deleteSession(pomo.time)}>
+                  delete
+                </button>
+              </div>
             </div>
-            <p>
-              {new Date(pomo.time).toLocaleDateString("pt-BR", {
-                day: "2-digit",
-                month: "2-digit",
-                year: "numeric",
-                hour: "2-digit",
-                minute: "2-digit",
-              })}
-            </p>
-            <button onClick={() => deleteSession(pomo.time)}>
-              delete
-            </button>
           </li>
         ))}
       </ul>
-    </div>
+    </div >
   )
 }
