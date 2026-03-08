c75a6d6006fa3df8691c05140751082a77b56a72
levith <levith.dev@gmail.com>
2026-03-08 09:41:11 -0300

    refactor: extract filter logic into useFilterPomoList hook and simplify SelectorFilter component

---
+++ src/components/SelectorFilter.tsx
@@ -0,0 +1,25 @@
+import type React from "react"
+import type { FilterType } from "../types/FilterType"
+
+export interface SelectorFilterProps {
+  actions: {
+    setFilterType: React.Dispatch<React.SetStateAction<FilterType>>
+  }
+}
+
+export function SelectorFilter({ actions }: SelectorFilterProps) {
+  const { setFilterType } = actions
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterType(e.target.value as FilterType)
+  }
+
+  return (
+    <div>
+      <select onChange={handleChange} className="border border-black">
+        <option value="all">all</option>
+        <option value="7days">last 7 days</option>
+      </select>
+    </div>
+  )
+}
